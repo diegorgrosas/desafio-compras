22bfbbf6a72d87459653d99ec1a858eb3aa864a1
Diego Rosas <diegorosas19@gmail.com>
2025-06-25 11:13:21 -0300

    Menu inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao = 1;
+
+        while (opcao != 0){
+            System.out.print("Digite 0 para sair ou 1 para continuar: ");
+            opcao = scanner.nextInt();
+            if (opcao == 1) {
+                System.out.println("Escolheu continuar!");
+            } else if (opcao != 0) {
+                System.out.println("Opção inválida!");
+            }
+        }
+
     }
 }
